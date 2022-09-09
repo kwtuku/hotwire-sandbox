cccfeab00d91cb8eb18ac5b57d547787a81f5117
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-09-09 21:29:26 +0900

    chore: コマンドで生成される rspec のファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module HotwireSandbox
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        helper_specs: false,
+        request_specs: false,
+        routing_specs: false,
+        view_specs: false
+    end
   end
 end
