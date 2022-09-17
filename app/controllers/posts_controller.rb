5de01f49a01814bfc40ee97e021f91aa71968a0e
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-09-17 22:29:35 +0900

    feat: 表示される文言を日本語化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to post_path(@post), notice: 'Post was successfully created.'
+      redirect_to post_path(@post), notice: t('general.created', resource_name: Post.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to post_path(@post), notice: 'Post was successfully updated.'
+      redirect_to post_path(@post), notice: t('general.updated', resource_name: Post.model_name.human)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy!
-    redirect_to posts_path, notice: 'Post was successfully destroyed.'
+    redirect_to posts_path, notice: t('general.destroyed', resource_name: Post.model_name.human)
   end
 
   private
