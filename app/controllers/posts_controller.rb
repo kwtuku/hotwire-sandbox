c0ddc3973b92f8cef571d3dc8b1daf4196dd9dbb
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-09-30 21:36:39 +0900

    feat: 投稿を作成するときの処理を turbo stream 化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to post_path(@post), notice: t('general.created', resource_name: Post.model_name.human)
+      flash.now.notice = t('general.created', resource_name: Post.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
