ac654ae8cc334e53506347a7aab81ba9eb92e98f
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-08 23:11:00 +0900

    chore: シードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Post.create!(
+    [
+      { content: 'a' * 300 },
+      { content: 'おはよう' },
+      { content: 'こんにちは' },
+      { content: 'こんばんは' },
+      { content: 'アンニョンハセヨ' },
+      { content: 'オラ' },
+      { content: 'ナマステ' },
+      { content: 'ニーハオ' },
+      { content: 'ボンジュール' },
+      { content: 'ボンジョルノ' }
+    ]
+  )
+end
