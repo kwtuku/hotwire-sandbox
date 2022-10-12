ccb739bcf64302d3b5ee75150bf52a7a9593be8d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-12 12:39:35 +0900

    test: 投稿の詳細ページに返信の一覧が表示されるようになったので、返信の一覧表示のテストを修正

--- spec/system/posts/replies_spec.rb
+++ spec/system/posts/replies_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe '返信', type: :system do
 
     it '一覧が表示される' do
       visit root_path
+      find("[data-testid='post-link-#{parent_post.id}']").click
       expect(page).to have_content 'こんにちは'
-      click_link '2'
+      expect(page).to have_current_path post_path(parent_post)
       expect(page).to have_content 'オラ'
       expect(page).to have_content 'ボンジョルノ'
-      expect(page).to have_current_path post_posts_path(parent_post)
     end
   end
 
