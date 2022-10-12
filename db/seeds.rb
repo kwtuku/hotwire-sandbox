c47eb3e086e87013f1f1af54b1e871ff69721141
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-12 22:17:53 +0900

    chore: シードで子、孫レコードを持つレコードが作れるよう変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,26 @@
+contents = [
+  { content: 'a' * 300 },
+  { content: 'おはよう' },
+  { content: 'こんにちは' },
+  { content: 'こんばんは' },
+  { content: 'アンニョンハセヨ' },
+  { content: 'オラ' },
+  { content: 'ナマステ' },
+  { content: 'ニーハオ' },
+  { content: 'ボンジュール' },
+  { content: 'ボンジョルノ' }
+]
+
 10.times do
-  Post.create!(
-    [
-      { content: 'a' * 300 },
-      { content: 'おはよう' },
-      { content: 'こんにちは' },
-      { content: 'こんばんは' },
-      { content: 'アンニョンハセヨ' },
-      { content: 'オラ' },
-      { content: 'ナマステ' },
-      { content: 'ニーハオ' },
-      { content: 'ボンジュール' },
-      { content: 'ボンジョルノ' }
-    ]
-  )
+  Post.create!(contents.shuffle)
+end
+
+Post.roots.last(10).each_with_index do |post, index|
+  post.children.create!(contents.shuffle)
+
+  next unless index >= 7
+
+  post.children.each do |child_post|
+    child_post.children.create!(contents.shuffle)
+  end
 end
