5b78d2bf439b7c6ef76bc9df213f844935e514c9
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-13 17:00:20 +0900

    feat: 投稿の詳細ページに祖先の投稿が表示されるよう変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: redirect('/posts')
   resources :posts do
+    resources :ancestors, only: %i[index], module: :posts
     resources :replies, only: %i[index new create], module: :posts, as: :posts
   end
 end
