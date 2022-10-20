aae5c4de2649e25f4947e4d4d345427a2b2af29a
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-20 23:05:11 +0900

    feat: data-turbo-confirm-button 属性で確認モーダルのボタンの文言を指定できるよう変更

--- app/javascript/custom_turbo_confirm.js
+++ app/javascript/custom_turbo_confirm.js
@@ -1,12 +1,14 @@
 import { Modal } from 'bootstrap';
 
-const newConfirmMethod = (message) => {
+const newConfirmMethod = (message, formElement) => {
   const modalElement = document.getElementById('turbo-confirm-modal');
   const modal = new Modal(modalElement);
   const messageElement = document.getElementById('turbo-confirm-modal-message');
   const confirmButtonElement = document.getElementById('turbo-confirm-modal-confirm-button');
+  const confirmButtonText = formElement.getAttribute('data-turbo-confirm-button');
 
   messageElement.textContent = message;
+  if (confirmButtonText !== null) confirmButtonElement.textContent = confirmButtonText;
   modal.show();
 
   return new Promise((resolve) => {
