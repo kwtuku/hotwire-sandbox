35b4cc4e5ca139b6269e1631ac665840f9bfac55
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-10-25 18:36:17 +0900

    feat: ユーザーと投稿の関連付けを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   devise :confirmable, :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
 
+  has_many :posts, dependent: :destroy
+
   validates :name, presence: true
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
