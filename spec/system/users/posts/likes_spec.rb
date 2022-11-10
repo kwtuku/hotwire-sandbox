6a46949c477f2e3b2266d42747bf4dc3eaa08b49
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-11-10 13:39:50 +0900

    test: 投稿の詳細ページからいいねする場合のテストを追加

--- spec/system/users/posts/likes_spec.rb
+++ spec/system/users/posts/likes_spec.rb
@@ -14,18 +14,40 @@ RSpec.describe 'いいね', type: :system do
   end
 
   describe '作成、削除' do
-    it 'ボタンが切り替わる' do
-      sign_in user
-      visit root_path
-      find("[data-testid='post-like-button-#{post.id}']").click
+    context '投稿の一覧ページの場合' do
+      it 'ボタンが切り替わる、いいね数が表示される' do
+        sign_in user
+        visit root_path
+        find("[data-testid='post-like-button-#{post.id}']").click
 
-      expect(page).to have_selector "[data-testid=post-unlike-button-#{post.id}]"
-      expect(page).to have_selector "[data-testid=post-likes-count-#{post.id}]", text: '1'
+        expect(page).to have_selector "[data-testid=post-unlike-button-#{post.id}]"
+        expect(page).to have_selector "[data-testid=post-likes-count-#{post.id}]", text: '1'
 
-      find("[data-testid='post-unlike-button-#{post.id}']").click
+        find("[data-testid='post-unlike-button-#{post.id}']").click
 
-      expect(page).to have_selector "[data-testid=post-like-button-#{post.id}]"
-      expect(page).to have_selector "[data-testid=post-likes-count-#{post.id}]", text: '0'
+        expect(page).to have_selector "[data-testid=post-like-button-#{post.id}]"
+        expect(page).to have_selector "[data-testid=post-likes-count-#{post.id}]", text: '0'
+      end
+    end
+
+    context '投稿の詳細ページの場合' do
+      it 'ボタンが切り替わる、いいね数が表示される' do
+        sign_in user
+        visit root_path
+        find("[data-testid='post-link-#{post.id}']").click
+
+        expect(page).to have_content '投稿'
+
+        find("[data-testid='post-like-button-#{post.id}']").click
+
+        expect(page).to have_selector "[data-testid=post-unlike-button-#{post.id}]"
+        expect(page).to have_content '1 件のいいね'
+
+        find("[data-testid='post-unlike-button-#{post.id}']").click
+
+        expect(page).to have_selector "[data-testid=post-like-button-#{post.id}]"
+        expect(page).to have_content :all, '0 件のいいね'
+      end
     end
   end
 end
